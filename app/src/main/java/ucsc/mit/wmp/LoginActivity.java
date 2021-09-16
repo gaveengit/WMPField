9dd6c16c7d109071c74514589c223f37bb8fddc8
Gaveen <gaveengit@gmail.com>
2021-09-16 09:38:01 +0530

    login window

---
+++ app/src/main/java/ucsc/mit/wmp/LoginActivity.java
@@ -0,0 +1,21 @@
+package ucsc.mit.wmp;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_window);
+    }
+}
